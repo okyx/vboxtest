c515fc8c825d86c45c670ac32a147428e4dd8293
Okyx <zicolavladimir@mhs.mdp.ac.id>
2022-11-06 20:24:01 +0700

    tes

--- test.py
+++ test.py
@@ -2,23 +2,16 @@ from apache_beam.options.pipeline_options import PipelineOptions
 import apache_beam as beam
 import re
 import os
-if os.path.exists('count.txt-00000-of-00001'):
+if os.path.exists('count.txt-00000-of-00002'):
     os.remove('count.txt-00000-of-00001')
+    os.remove('count.txt-00001-of-00002')
 output_path = 'count.txt'
 beam_options = PipelineOptions([
    "--runner=PortableRunner",
-    "--job_endpoint=localhost:8099",
-    "--environment_type=LOOPBACK",
-    # "--environment_config=localhost:50000"
+    "--job_endpoint=192.168.0.165:8099"
+    "--environtment_type=LOOPBACK"
 ])
-with beam.Pipeline() as p:
-    (
-        p
-        | beam.Create(range(100))
-        | beam.Map(lambda x : 'genap' if x%2==0 else 'ganjil')
-        | beam.combiners.Count.PerElement()
-        | beam.Map(print)
-    )
+
 with beam.Pipeline() as p:
     (
         p
